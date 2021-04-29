b8492d4fcb55b15011985cb49d997d82a017f240
kuwizard <kuzwiz@mail.ru>
2021-04-29 10:30:18 +0100

    [39857] setting gamedatas handcount parameter each time it changes to follow up with Panic/Cat Balou

--- modules/js/PlayerTrait.js
+++ modules/js/PlayerTrait.js
@@ -1,5 +1,5 @@
-function truncate(str, n){
-  return (str.length > n) ? str.substr(0, n-1) + '&hellip;' : str;
+function truncate(str, n) {
+  return str.length > n ? str.substr(0, n - 1) + '&hellip;' : str;
 }
 
 define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
@@ -23,7 +23,7 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
         player.newNo = player.no;
         player.shortName = truncate(player.name, 11);
 
-        player.background = player.color_back? ('#' + player.color_back) : 'transparent';
+        player.background = player.color_back ? '#' + player.color_back : 'transparent';
         dojo.place(this.format_block('jstpl_player', player), 'board');
         this.addTooltipHtml('player-character-' + player.id, this.format_block('jstpl_characterTooltip', player));
         player.inPlay.forEach((card) => this.addCard(card, 'player-inplay-' + player.id));
@@ -194,6 +194,7 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
       //        newHandCount = currentHandCount + parseInt(amount);
       dojo.attr('bang-player-' + playerId, 'data-hand', newHandCount);
       dojo.attr('bang-player-board-' + playerId, 'data-hand', newHandCount);
+      this.gamedatas.players[playerId].handCount = newHandCount;
     },
 
     displayPlayersHelp() {
