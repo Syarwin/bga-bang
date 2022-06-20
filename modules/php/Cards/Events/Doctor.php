c6276c358501b7d4baf3306760d9e99acb07c821
Pavel Kulagin <kuzwiz@mail.ru>
2022-06-20 17:37:26 +0100

    [feature] supported Doctor card

--- modules/php/Cards/Events/Doctor.php
+++ modules/php/Cards/Events/Doctor.php
@@ -1,5 +1,6 @@
 <?php
 namespace BANG\Cards\Events;
+use BANG\Managers\Players;
 use BANG\Models\AbstractEventCard;
 
 class Doctor extends AbstractEventCard
@@ -16,6 +17,11 @@ class Doctor extends AbstractEventCard
 
   public function resolveEffect()
   {
-
+    $players = Players::getLivingPlayers();
+    $minHp = min($players->map(function ($player) { return $player->getHp(); })->toArray());
+    $playersWithMinHp = $players->filter(function ($player) use ($minHp) { return $player->getHp() === $minHp; });
+    foreach ($playersWithMinHp as $player) {
+      $player->gainLife(1);
+    }
   }
 }
