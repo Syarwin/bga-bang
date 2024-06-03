bb279d35d7853c24302bd71943b869d21aa23176
Pavel Kulagin <kuzwiz@mail.ru>
2024-06-03 17:58:35 +0100

    [fix] Blood Brothers should work before every players turn, not before Sheriff's turn all at once

--- modules/php/Cards/Events/BloodBrothers.php
+++ modules/php/Cards/Events/BloodBrothers.php
@@ -1,7 +1,6 @@
 <?php
 namespace BANG\Cards\Events;
 use BANG\Core\Stack;
-use BANG\Managers\Players;
 use BANG\Models\AbstractEventCard;
 use BANG\Models\Player;
 
@@ -12,8 +11,8 @@ class BloodBrothers extends AbstractEventCard
     parent::__construct($id);
     $this->type = CARD_BLOOD_BROTHERS;
     $this->name = clienttranslate('Blood Brothers');
-    $this->text = clienttranslate('Each player may choose to lose one of his life points to give to another player at the beginning of his turn.');
-    $this->effect = EFFECT_INSTANT;
+    $this->text = clienttranslate('At the beginning of his turn, each player may lose one life point (except the last one) to give one life point to any player of his choice.');
+    $this->effect = EFFECT_STARTOFTURN;
     $this->expansion = FISTFUL_OF_CARDS;
   }
 
@@ -23,11 +22,7 @@ class BloodBrothers extends AbstractEventCard
    */
   public function resolveEffect($player = null)
   {
-    $players = Players::getLivingPlayersStartingWith($player);
-
-    foreach (array_reverse($players->toArray()) as $player) {
-      $atom = Stack::newSimpleAtom(ST_BLOOD_BROTHERS, $player->getId());
-      Stack::insertOnTop($atom);
-    }
+    $atom = Stack::newSimpleAtom(ST_BLOOD_BROTHERS, $player->getId());
+    Stack::insertOnTop($atom);
   }
 }
